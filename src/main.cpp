3f2ff2644aaab4ed5f4806bc204313a327212150
Mess851 <143230121+Mess851@users.noreply.github.com>
2023-09-02 01:05:32 +0000

    Clase de Viernes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <Mascota.hpp>
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Mensaje"<<std::endl;
+    std::cout << "Juego de Mascotas" << std::endl;
+
+    Mascota m1,m2,m3;
+
+    m1.EstablecerNombre("Rudeus");
+    m2.EstablecerNombre("Rengar");
+    m3.EstablecerNombre("Zeus");
+
+    m1.DecirNombre();
+    m2.DecirNombre();
+    m3.DecirNombre();
 
     return 0;
 }
